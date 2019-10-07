5339b40417d9d018cea585f3036a25c34bcb3b72
Unai <uahedo002@ikasle.ehu.eus>
2019-10-07 15:33:52 +0200

    Laboratorio 2A terminado

--- php/Layout.php
+++ php/Layout.php
@@ -8,7 +8,10 @@
   <section class="main" id="s1">
     <div>
 
-      <h2>Quiz: el juego de las preguntas</h2>
+      <h2>Quiz: el juego de las preguntas</h2><br>
+        <a href="../php/QuestionForm.php">Clica aquí para crear una pregunta.</a><p></p><br/>
+        <a href="../php/QuestionFormHtml5.php">Clica aquí para crear una pregunta (HTML5).</a><p></p><br/>
+        <a href="../php/QuestionFormWithImage.php">Clica aquí para crear una pregunta con fotografía.</a><p></p><br>
       
     </div>
   </section>
